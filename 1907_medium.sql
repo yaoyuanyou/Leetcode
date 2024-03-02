6f8346f3083b59d33bd73256b9a04e29553b2cb4
Tony Yao <72855371+yaoyuanyou@users.noreply.github.com>
2024-03-02 16:16:37 -0500

    LC1907

---
+++ 1907_medium.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/count-salary-categories/
+SELECT 
+    'Low Salary' AS category,
+    SUM(IF(income < 20000, 1, 0))AS accounts_count
+FROM Accounts
+UNION
+SELECT 
+    'Average Salary' AS category,
+    SUM(IF(income BETWEEN 20000 AND 50000, 1, 0))AS accounts_count
+FROM Accounts
+UNION
+SELECT 
+    'High Salary' AS category,
+    SUM(IF(income > 50000, 1, 0))AS accounts_count
+FROM Accounts; 
