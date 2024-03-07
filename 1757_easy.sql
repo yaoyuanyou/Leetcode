35086327af56f37445b9a2164dad1ad550b07778
Tony Yao <72855371+yaoyuanyou@users.noreply.github.com>
2024-03-07 14:10:10 -0500

    LC1757

---
+++ 1757_easy.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
