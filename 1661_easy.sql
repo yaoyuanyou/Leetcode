f33bfbb0965f4c092c646359bfab5d6492e3dfae
Tony Yao <72855371+yaoyuanyou@users.noreply.github.com>
2024-03-07 16:15:20 -0500

    LC1661

---
+++ 1661_easy.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/average-time-of-process-per-machine/?envType=study-plan-v2&envId=top-sql-50
+SELECT machine_id, ROUND(SUM(timestamp)/SUM(num/2), 3) AS processing_time
+FROM (
+    SELECT machine_id, activity_type,
+    CASE
+    WHEN activity_type = 'start' THEN -SUM(timestamp)
+    ELSE SUM(timestamp)
+    END AS timestamp,
+    COUNT(activity_type) AS num
+    FROM Activity
+    GROUP BY machine_id, activity_type
+) a
+GROUP BY machine_id;
