d29f478af4fbaa757d460b26a6344192912e37fd
Tony Yao <72855371+yaoyuanyou@users.noreply.github.com>
2024-03-07 14:11:21 -0500

    LC595

---
+++ 595_easy.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/big-countries/?envType=study-plan-v2&envId=top-sql-50
+SELECT name,
+    population,
+    area
+FROM World
+WHERE area >=3000000 OR population >= 25000000;
