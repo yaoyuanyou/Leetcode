04f17be92c28d89a9cb68a83ace578fe992a14e6
Tony Yao <72855371+yaoyuanyou@users.noreply.github.com>
2024-03-08 00:24:09 -0500

    LC577

---
+++ 577_easy.sql
@@ -0,0 +1,7 @@
+SELECT machine_id,
+    ROUND(AVG(CASE
+            WHEN activity_type = 'start' THEN -timestamp
+            ELSE timestamp
+            END) * 2, 3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
