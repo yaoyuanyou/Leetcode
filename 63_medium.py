2383b3648906595b3714ebba3544b88fc1dc4a2b
Tony Yao <72855371+yaoyuanyou@users.noreply.github.com>
2024-03-27 16:28:19 -0400

    Update one solution

--- 63_medium.py
+++ 63_medium.py
@@ -26,6 +26,34 @@ class Solution:
 
         return self.uniquePathsWithObstacles([obstacleGrid[x][1:] for x in range(m)]) + self.uniquePathsWithObstacles(obstacleGrid[1:])
 
+#Leetcode NOT accepted, but works fine at local.
+class Solution:
+    dp = dict()
+    def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        if m == 1 and n == 1:
+            if obstacleGrid[0][0] == 0:
+                self.dp[(m, n)] = 1
+            else:
+                self.dp[(m, n)] = 0
+        elif obstacleGrid[0][0] == 1 or obstacleGrid[m - 1][n - 1] == 1:
+            self.dp[(m, n)] = 0
+        elif m == 1:
+            if (m, n - 1) not in self.dp.keys():
+                self.dp[(m, n - 1)] = self.uniquePathsWithObstacles([obstacleGrid[0][1:]])
+            self.dp[(m, n)] = self.dp[(m, n - 1)]
+        elif n == 1:
+            if (m - 1, n) not in self.dp.keys():
+                self.dp[(m - 1, n)] = self.uniquePathsWithObstacles(obstacleGrid[1:])
+            self.dp[(m, n)] = self.dp[(m - 1, n)]
+        else:
+            if (m, n - 1) not in self.dp.keys():
+                self.dp[(m, n - 1)] = self.uniquePathsWithObstacles([obstacleGrid[x][1:] for x in range(m)])
+            if (m - 1, n) not in self.dp.keys():
+                self.dp[(m - 1, n)] = self.uniquePathsWithObstacles(obstacleGrid[1:])
+            self.dp[(m, n)] = self.dp[(m, n - 1)] + self.dp[(m - 1, n)]
+        return self.dp[(m, n)]
+
 #fast
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
