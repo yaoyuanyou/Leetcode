baebd6876caebe43eea66f49cb2b461b70d50ccd
Tony Yao <72855371+yaoyuanyou@users.noreply.github.com>
2024-07-18 21:11:46 -0400

    Optimized Space Complexity

--- 2487_medium.py
+++ 2487_medium.py
@@ -16,3 +16,42 @@ class Solution:
             return head
         else:
             return compare
+
+
+#Space Complexity O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        maximum = prev.val
+
+        curr = prev
+         
+        while curr.next:
+            if curr.next.val >= curr.val:
+                maximum = curr.next.val
+                curr = curr.next
+            else:
+                curr.next = curr.next.next
+        curr.next = None
+        
+        curr = prev
+        reverse = None
+        while curr:
+            temp = curr.next
+            curr.next = reverse
+            reverse = curr
+            curr = temp
+
+        return reverse
